074fb2bf8050e1285660edf91527d60cca046f65
Isaac Castro <i.castro@cignuz.com>
2022-06-06 15:07:37 -0500

    UI Layout

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -7,7 +7,9 @@ const PlayerLayout = ({ children }) => {
       <Box position="absolute" top="0" width="250px" left="0">
         <Sidebar />
       </Box>
-      <Box marginLeft="250px">{children}</Box>
+      <Box marginLeft="250px">
+        <Box height="calc(100vh - 100px)">{children}</Box>
+      </Box>
       <Box position="absolute" left="0" bottom="0">
         player
       </Box>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,56 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { Box, Text, Flex, Image } from "@chakra-ui/react";
+import GradientLayout from "../components/gradientLayout";
+import prisma from "../lib/prisma";
 
-export default function Home() {
-  return <div>Home</div>;
-}
+const Home = ({ artists }) => {
+  return (
+    <GradientLayout
+      color="blue"
+      title="Isaac Castro"
+      subtitle="profile"
+      description="15 public playlists"
+      image="https://i.pinimg.com/originals/79/80/9d/79809debe02ca1f02727c080397a34a7.jpg"
+      roundImage
+    >
+      <Box color="white" paddingX="40px" width="100%">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artists this month
+          </Text>
+          <Text fontSize="md">Only visible to you</Text>
+        </Box>
+        <Flex gap="10px">
+          {artists.map((artist) => (
+            <Box
+              key={artist.id}
+              width="200px"
+              bg="gray.800"
+              borderRadius="4px"
+              padding="15px"
+            >
+              <Image
+                src="https://placekitten.com/300/300"
+                borderRadius="100%"
+              />
+              <Box marginTop="20px">
+                <Text fontSize="large">{artist.name}</Text>
+                <Text fontSize="x-small">Artist</Text>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
+    </GradientLayout>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+  return {
+    props: {
+      artists,
+    },
+  };
+};
+
+export default Home;
