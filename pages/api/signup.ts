15794759ff8ed01e2775c033c8a9bf412d27ada2
Isaac Castro <i.castro@cignuz.com>
2022-07-03 02:49:57 -0500

    User model update

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,8 +1,8 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import cookie from "cookie";
-import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "../../lib/prisma";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../lib/prisma';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const salt = bcrypt.genSaltSync();
@@ -19,7 +19,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     });
   } catch (e) {
     res.status(401);
-    res.json({ error: "User already exists" });
+    res.json({ error: 'User already exists' });
     return;
   }
   const token = jwt.sign(
@@ -28,18 +28,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       id: user.id,
       time: Date.now(),
     },
-    "hello",
-    { expiresIn: "8h" }
+    'hello',
+    { expiresIn: '8h' }
   );
 
   res.setHeader(
-    "Set-Cookie",
-    cookie.serialize("TRAX_ACCESS_TOKEN", token, {
+    'Set-Cookie',
+    cookie.serialize('TRAX_ACCESS_TOKEN', token, {
       httpOnly: true,
       maxAge: 8 * 60 * 60,
-      path: "/",
-      sameSite: "lax",
-      secure: process.env.NODE_ENV === "production",
+      path: '/',
+      sameSite: 'lax',
+      secure: process.env.NODE_ENV === 'production',
     })
   );
 
---
+++ prisma/migrations/20220703074129_user_nullable_fields/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "updatedAt" DROP NOT NULL,
+ALTER COLUMN "firstName" DROP NOT NULL,
+ALTER COLUMN "lastName" DROP NOT NULL;
