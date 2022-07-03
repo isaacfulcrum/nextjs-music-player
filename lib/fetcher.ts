e5185e2d26f92381cac979a4c0bc40fb57a12987
Isaac Castro <i.castro@cignuz.com>
2022-07-03 02:55:44 -0500

    Lint fetcher

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,9 +1,9 @@
 export default function fetcher(url: string, data = undefined) {
   return fetch(`${window.location.origin}/api${url}`, {
-    method: data ? "POST" : "GET",
-    credentials: "include",
+    method: data ? 'POST' : 'GET',
+    credentials: 'include',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
   }).then((res) => {
