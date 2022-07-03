3ed45de441eef554c73f03ffae623860ef4e9715
Isaac Castro <i.castro@cignuz.com>
2022-07-03 02:19:07 -0500

    Fix store type error

--- lib/store.ts
+++ lib/store.ts
@@ -3,10 +3,10 @@ import { createStore, action } from 'easy-peasy';
 export const store = createStore({
   activeSongs: [],
   activeSong: null,
-  changeActiveSongs: action((state, payload) => {
+  changeActiveSongs: action((state: any, payload) => {
     state.activeSongs = payload;
   }),
-  changeActiveSong: action((state, payload) => {
+  changeActiveSong: action((state: any, payload) => {
     state.activeSong = payload;
   }),
 });
